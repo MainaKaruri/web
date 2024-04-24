252a392b9ca28428f1bf68c1bf98faf8f1e779f4
MainaKaruri <156428676+MainaKaruri@users.noreply.github.com>
2024-04-24 08:14:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const header = document.querySelector("header");
-window.addEventListener("scroll", function () {
-    header.classList.toggle("sticky", window.scrollY > 100);
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", this.window.scrollY > 0);
+})
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+section.forEach(sec => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if (top => offset && top < offset + height) {
+        navlinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+        })
+    }
 });
